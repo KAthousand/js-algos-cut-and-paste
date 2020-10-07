8efada932a42ac08863e85c862d600d9031b09d8
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-07 17:45:53 -0400

    Kyle Thousand

--- cutAndPaste.js
+++ cutAndPaste.js
@@ -1,11 +1,15 @@
-//  CUT AND PASTE - 
+//  CUT AND PASTE -
 
 const cutAndPaste = (arr, num) => {
+  let newArr = arr.splice(num);
+  arr.forEach((value) => {
+    newArr.push(value);
+  });
+  return newArr;
+};
 
-}
-
-console.log(cutAndPaste([1, 2, 5, "help", "wow", 3], 1))
+console.log(cutAndPaste([1, 2, 5, "help", "wow", 3], 1));
 
 module.exports = {
-  cutAndPaste
+  cutAndPaste,
 };
